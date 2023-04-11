857ef18cf2eb4d9339041951bf613cebde358a15
Marc Lacoursiere <marc@roosoft.com>
2023-04-11 16:11:31 -0400

    updated neovim input in the flake

--- flake.nix
+++ flake.nix
@@ -15,7 +15,10 @@
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
     # nix-colors.url = "github:misterio77/nix-colors";
-    neovim.url = "github:notashelf/neovim-flake";
+    neovim-flake = {
+      url = "github:notashelf/neovim-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs: {
