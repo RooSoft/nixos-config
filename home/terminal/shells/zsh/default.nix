e95f32e6aa4bfaf24284b0ec420ebb46d227c641
Marc Lacoursiere <marc@roosoft.com>
2023-04-12 09:41:46 -0400

    added update-system and update-user aliases

--- home/terminal/shells/zsh/default.nix
+++ home/terminal/shells/zsh/default.nix
@@ -9,6 +9,10 @@
 
   programs.zsh = {
     enable = true;
+    shellAliases = {
+      update-system = "sudo nixos-rebuild switch --flake ~/.config/nixpkgs/.#nixos-mini";
+      update-user = "home-manager switch --flake ~/.config/nixpkgs/.#roo@nixos-mini";
+    };
     plugins = [
       {
         name = "powerlevel10k";
