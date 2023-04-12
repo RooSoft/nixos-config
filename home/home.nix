56cb5fdcb1efd15233e48b49cb406d0b507df315
Marc Lacoursiere <marc@roosoft.com>
2023-04-12 07:34:04 -0400

    moved zsh config in its own file

--- home/home.nix
+++ home/home.nix
@@ -1,42 +1,29 @@
-{ lib, config, pkgs, inputs, ... }:
-
-let
+{
+  lib,
+  config,
+  pkgs,
+  inputs,
+  ...
+}: let
   unstable = import <nixos-unstable> {};
+in {
+  imports = [
+    ./terminal/editors/neovim
+    ./terminal/shells/zsh
+  ];
 
-in
-  {
-    imports = [
-      ./terminal/editors/neovim
+  nixpkgs.config.allowUnfree = true;
+
+  home = {
+    stateVersion = "22.11";
+    username = "roo";
+    homeDirectory = /home/roo;
+    packages = with pkgs; [
+      htop
+      tmux
+      nodejs-16_x
+      bash
+      unrar-wrapper
     ];
-
-    nixpkgs.config.allowUnfree = true;
-
-    home = {
-      stateVersion = "22.11";
-      username = "roo";
-      homeDirectory = /home/roo;
-      packages = with pkgs; [
-        htop
-        tmux
-        nodejs-16_x
-        bash
-        unrar-wrapper
-      ];
-    };
-
-    programs.zsh = {
-      enable = true;
-      plugins = [
-        {
-          name = "powerlevel10k";
-          src = pkgs.zsh-powerlevel10k;
-          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-        }
-        {
-          name = "powerlevel10k-config";
-          src = lib.cleanSource ./p10k-config;
-          file = "p10k.zsh";
-        }
-      ];
-    };
-  }
+  };
+}
---
+++ home/terminal/shells/zsh/default.nix
@@ -0,0 +1,25 @@
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [
+  ];
+
+  programs.zsh = {
+    enable = true;
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = lib.cleanSource ./p10k-config;
+        file = "p10k.zsh";
+      }
+    ];
+  };
+}
