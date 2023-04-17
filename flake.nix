f6d0d746bc5af37f2a4ada7e576515d5b6cce72e
Marc Lacoursiere <marc@roosoft.com>
2023-04-17 08:17:46 -0400

    added the roo@nixos-lenovo user

--- flake.nix
+++ flake.nix
@@ -51,11 +51,14 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      # FIXME replace with your username@hostname
       "roo@nixos-mini" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         extraSpecialArgs = {inherit inputs pkgs_unstable;}; # Pass flake inputs to our config
-        # > Our main home-manager configuration file <
+        modules = [./home/home.nix];
+      };
+      "roo@nixos-lenovo" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        extraSpecialArgs = {inherit inputs pkgs_unstable;}; # Pass flake inputs to our config
         modules = [./home/home.nix];
       };
     };
