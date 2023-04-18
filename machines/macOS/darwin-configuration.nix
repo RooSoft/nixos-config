089d0da7935b0780c3276ca3e1094036085d6558
Marc Lacoursiere <marc@roosoft.com>
2023-04-18 14:30:37 -0400

    enable flakes for darwin

--- machines/macOS/darwin-configuration.nix
+++ machines/macOS/darwin-configuration.nix
@@ -25,4 +25,8 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
+
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
 }
