8f3cfc2ea1f508f56b923867f8633b2c1ed71974
Marc Lacoursiere <marc@roosoft.com>
2023-04-18 13:00:53 -0400

    removed useless comments

--- flake.nix
+++ flake.nix
@@ -15,12 +15,6 @@
 
     flake-utils.url = "github:numtide/flake-utils";
 
-    # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
-
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
     neovim-flake = {
       url = "github:roosoft/neovim-flake";
       inputs.nixpkgs.follows = "nixpkgs";
