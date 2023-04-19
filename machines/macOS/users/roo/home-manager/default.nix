943ac7247107096fd8fd59176528d76ed2c9bb27
Marc Lacoursiere <marc@roosoft.com>
2023-04-19 13:28:25 -0400

    added quite a bit of starship configuration

--- machines/macOS/users/roo/home-manager/default.nix
+++ machines/macOS/users/roo/home-manager/default.nix
@@ -1,6 +1,6 @@
 # options docs found here: https://nix-community.github.io/home-manager/options.html
 
-{ pkgs, ... } : 
+{ pkgs, lib, ... } : 
 {
   home = {
     stateVersion = "22.11";
@@ -10,6 +10,12 @@
         curl
         less
     ];
+
+    sessionVariables = {
+      SPACESHIP_HOST_SHOW = "always";
+      SPACESHIP_USER_SHOW = "always";
+      SPACESHIP_TIME_SHOW = "always";
+    };
   };
 
   programs = {
@@ -35,6 +41,58 @@
       enableZshIntegration = true;
 
       settings = {
+        palette = "foo";
+
+        palettes.foo = {
+          mustard = "#af8700";
+        };
+
+        fill.symbol = " ";
+
+        username = {
+          show_always = true;
+          style_user = "mustard bold";
+          style_root = "bright-red bold";
+          format = "[$user]($style)";
+        };
+
+        hostname = {
+          ssh_only = false;
+          ssh_symbol = " 📞";
+          style = "mustard bold";
+          format = "@[$hostname]($style)$ssh_symbol ";
+        };
+
+        directory = {
+          truncation_length = 3;
+          truncate_to_repo = false;
+          format = "[$read_only]($read_only_style)[$path]($style) ";
+          read_only = "🔒 ";
+        };
+
+        git_metrics.disabled = false;
+        git_status.style = "";
+
+        time = {
+          disabled = false;
+          format = "[$time]($style)";
+        };
+
+        format = lib.concatStrings[
+          "$username" 
+          "$hostname"
+          "$directory"
+          "$git_branch"
+          "$git_state"
+          "$git_status"
+          "$git_metrics"
+          "$fill"
+          "$cmd_duration"
+          "$jobs"
+          "$time"
+          "$line_break"
+          "❯ " 
+        ];
       };
     };
   };
