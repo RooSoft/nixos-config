471ae247a7770d6c510c1a735def7f937b9ce808
marc@roosoft.com <Marc Lacoursiere>
2023-04-23 12:38:16 -0400

    apply macoOS zsh config to nixos-mini as well

--- machines/nixos-mini/users/roo/terminal/shells/zsh/default.nix
+++ machines/nixos-mini/users/roo/terminal/shells/zsh/default.nix
@@ -9,6 +9,18 @@
 
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+    enableSyntaxHighlighting = true;
+
+    shellAliases = {
+      ls = "ls --color=auto -F";
+    };
+
+    sessionVariables = {
+      CLICOLOR = 1;
+      EDITOR = "nvim";
+    };
+
     shellAliases = {
       update-system = "sudo nixos-rebuild switch --flake ~/.config/nixpkgs/.#nixos-mini";
       update-user = "home-manager switch --flake ~/.config/nixpkgs/.#roo@nixos-mini";
