061bb8ac19a5e4462cb06d42128b1c8b1e775ccd
Marc Lacoursiere <roo@traveller.local>
2023-05-08 15:43:46 -0400

    added traveller

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
           specialArgs = { inherit inputs; modulesFolder = ./modules; }; # Pass flake inputs to our config
           # > Our main nixos configuration file <
           modules = [
-            ./machines/macOS/darwin-configuration.nix
+            ./machines/macOS/builder/darwin-configuration.nix
             home-manager.darwinModules.home-manager
             {
               home-manager = {
@@ -67,7 +67,30 @@
                 useUserPackages = true;
                 extraSpecialArgs = { commonFolder = ./common; };
                 users.roo = {
-                  imports = [ ./machines/macOS/users/roo/home-manager ];
+                  imports = [ ./machines/macOS/builder/users/roo/home-manager ];
+                };
+              };
+            }
+          ];
+        };
+
+        traveller = darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          pkgs = import nixpkgs {
+            system = "aarch64-darwin";
+          };
+          specialArgs = { inherit inputs; modulesFolder = ./modules; }; # Pass flake inputs to our config
+          # > Our main nixos configuration file <
+          modules = [
+            ./machines/macOS/traveller/darwin-configuration.nix
+            home-manager.darwinModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                extraSpecialArgs = { commonFolder = ./common; };
+                users.roo = {
+                  imports = [ ./machines/macOS/traveller/users/roo/home-manager ];
                 };
               };
             }
---
+++ machines/macOS/traveller/users/roo/home-manager/default.nix
@@ -0,0 +1,45 @@
+{ pkgs, lib, commonFolder, ... }:
+let
+  defaultHomePackagesPath = commonFolder + "/defaultHomePackages.nix";
+
+  modulesFolder = commonFolder + "/modules";
+
+  starshipModulePath = modulesFolder + "/starship.nix";
+  zshModulePath = modulesFolder + "/zsh.nix";
+in
+{
+  imports =
+    [
+      starshipModulePath
+      zshModulePath
+    ];
+
+  home = {
+    stateVersion = "22.11";
+
+    packages = with pkgs; (import defaultHomePackagesPath { pkgs = pkgs; }) ++ [
+      nodejs-16_x
+      elixir_1_14
+      elixir_ls
+      rnix-lsp
+      pinentry_mac
+      libfido2
+      yubico-piv-tool
+
+      rustc cargo rust-analyzer rustfmt
+    ];
+  };
+
+  programs = {
+    bat.enable = true;
+    bat.config.theme = "TwoDark";
+
+    zsh = {
+      shellAliases = {
+        ls = "ls --color=auto -F";
+        update-system = "darwin-rebuild switch --flake /Users/roo/.config/nix/.#";
+      };
+    };
+  };
+}
+
