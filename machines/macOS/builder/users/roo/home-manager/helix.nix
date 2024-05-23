8700c2da0cfcfb610086f118f678e1921801fb3d
Marc Lacoursiere <marc@roosoft.com>
2024-05-23 14:44:30 -0400

    streamlined helix configuration

--- machines/macOS/builder/users/roo/home-manager/helix.nix
+++ machines/macOS/builder/users/roo/home-manager/helix.nix
@@ -1,50 +1,6 @@
-{
-  pkgs,
-  unstable,
-  ...
-}: let
-  package = unstable.helix;
-
-  config = {
-    theme = "gruvbox_dark_hard";
-
-    editor = {
-      true-color = true;
-      line-number = "relative";
-      mouse = true;
-
-      cursor-shape = {
-        insert = "bar";
-        normal = "block";
-        select = "underline";
-      };
-
-      file-picker = {
-        hidden = false;
-      };
-
-      lsp = {
-        display-messages = true;
-      };
-
-      statusline = {
-        right = ["diagnostics" "position" "position-percentage" "file-encoding" "file-type"];
-      };
-    };
-
-    keys = {
-      normal = {
-        C-j = ["extend_to_line_bounds" "delete_selection" "paste_after"];
-        C-k = ["extend_to_line_bounds" "delete_selection" "move_line_up" "paste_before"];
-      };
-    };
-  };
-
-  tomlFormat = (pkgs.formats.toml {}).generate "something" config;
-in {
+{...}: {
   imports = [
+    ./config.nix
     ./languages.nix
   ];
-
-  home.file.".config/helix/config.toml".source = tomlFormat;
 }
