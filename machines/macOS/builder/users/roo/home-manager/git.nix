10edf147b1ef0b48f27d05c4efac22ae79aeec34
Marc Lacoursiere <marc@roosoft.com>
2024-06-26 20:10:10 -0400

    added some custom config for git delta

--- machines/macOS/builder/users/roo/home-manager/git.nix
+++ machines/macOS/builder/users/roo/home-manager/git.nix
@@ -2,6 +2,18 @@
   programs.git = {
     enable = true;
 
-    delta.enable = true;
+    delta = {
+      enable = true;
+
+      options = {
+        decorations = {
+          commit-decoration-style = "bold yellow box ul";
+          file-decoration-style = "none";
+          file-style = "bold yellow ul";
+        };
+        features = "decorations";
+        whitespace-error-style = "22 reverse";
+      };
+    };
   };
 }
