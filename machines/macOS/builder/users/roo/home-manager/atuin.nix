e6725be3d05c030ad31c22bd871085686a7b649b
Marc Lacoursiere <marc@roosoft.com>
2024-06-27 08:43:50 -0400

    some atuin config

--- machines/macOS/builder/users/roo/home-manager/atuin.nix
+++ machines/macOS/builder/users/roo/home-manager/atuin.nix
@@ -2,6 +2,25 @@
   programs.atuin = {
     enable = true;
     settings = {
+      auto_sync = false;
+      update_check = false;
+      filter_mode = "directory";
+      ctrl_n_shortcuts = true;
+      enter_accept = true;
+
+      stats = {
+        common_subcommands = [
+          "cargo"
+          "docker"
+          "git"
+          "ip"
+          "nix"
+          "npm"
+          "systemctl"
+        ];
+
+        common_prefix = ["sudo"];
+      };
     };
   };
 }
