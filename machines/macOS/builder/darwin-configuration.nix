3e9f3a4122cf7ca583f9b4e239a8da1f64957d69
Marc Lacoursiere <marc@roosoft.com>
2024-06-27 09:19:12 -0400

    some cleanup, including extracting open-webui in its own module

--- machines/macOS/builder/darwin-configuration.nix
+++ machines/macOS/builder/darwin-configuration.nix
@@ -4,7 +4,10 @@
   inputs,
   ...
 }: {
-  imports = [./postgresql.nix];
+  imports = [
+    ./postgresql.nix
+    ./open-webui.nix
+  ];
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
@@ -12,67 +15,16 @@
     tree-sitter
     wireguard-tools
 
-    npins
-
     inputs.nvim-flake.packages.aarch64-darwin.default
-
     inputs.project-commander.defaultPackage.aarch64-darwin
   ];
 
-  # networking.extraHosts =
-  # ''
-  # 192.168.10.1 edgerouter
-  # 192.168.10.2 edgeswitch
-  # 192.168.88.33 babar
-  # 127.0.0.1 ai
-
-  # 192.168.88.10 host01
-  # 192.168.88.11 host02
-  # 192.168.88.12 host03
-
-  # # 192.168.11.6 roosoft.com
-  # 192.168.11.6 git.roosoft.com
-  # # 192.168.11.6 wiki.roosoft.com
-
-  # # 192.169.13.5 electrum.roosoft.com
-
-  # 192.168.11.2 astro
-  # 192.168.11.3 loki
-  # 192.168.11.4 grafana
-  # 192.168.11.6 gitea #192.168.11.5
-  # 192.168.11.6 dmz
-  # 192.168.11.7 wiki
-  # 192.168.11.8 homepage
-  # 192.168.11.11 mempool
-  # 192.168.11.12 electrs
-  # 192.168.11.6 mattermost # 192.168.11.13
-  # 192.168.11.14 roadWarrior
-
-  # 192.168.12.2 ratel
-  # 192.168.12.3 loki-staging
-  # 192.168.12.4 grafana-staging
-  # 192.168.12.5 gitea-staging
-  # 192.168.12.7 wiki-staging
-  # 192.168.12.10 minio-staging
-  # 192.168.12.13 mattermost-staging
-
-  # 192.168.11.6 test-chat.roosoft.com
-  # '';
-
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
-
-  # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
 
-  # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh = {
     enable = true;
     # autosuggestion.enable = true;  #### NOT WORKING AS OF 24.05
   };
-  # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
@@ -84,43 +36,7 @@
     enable = true;
 
     brews = ["iproute2mac"];
-
     casks = ["wireshark" "1password-cli" "font-meslo-lg-nerd-font"];
-  };
-
-  launchd.daemons.open-webui = {
-    path = [
-      "/opt/homebrew/bin"
-      "/opt/homebrew/sbin"
-      "/Users/roo/.nix-profile/bin"
-      "/etc/profiles/per-user/roo/bin"
-      "/run/current-system/sw/bin"
-      "/nix/var/nix/profiles/default/bin"
-      "/usr/local/bin"
-      "/usr/bin"
-      "/usr/sbin"
-      "/bin"
-      "/sbin"
-    ];
-
-    environment = {
-      HOST = "0.0.0.0";
-      PORT = "8080";
-    };
-
-    script = ''
-      cd /Users/roo/.config/open-webui/backend
-      ${pkgs.bash}/bin/bash start.sh
-    '';
-
-    serviceConfig = {
-      UserName = "roo";
-      KeepAlive = true;
-      RunAtLoad = true;
-      RootDirectory = "/Users/roo/.config/open-webui/backend";
-      StandardOutPath = "/var/log/open-webui.out.log";
-      StandardErrorPath = "/var/log/open-webui.err.log";
-    };
   };
 
   nix.extraOptions = ''
---
+++ machines/macOS/builder/open-webui.nix
@@ -0,0 +1,36 @@
+{pkgs, ...} : {
+  launchd.daemons.open-webui = {
+    path = [
+      "/opt/homebrew/bin"
+      "/opt/homebrew/sbin"
+      "/Users/roo/.nix-profile/bin"
+      "/etc/profiles/per-user/roo/bin"
+      "/run/current-system/sw/bin"
+      "/nix/var/nix/profiles/default/bin"
+      "/usr/local/bin"
+      "/usr/bin"
+      "/usr/sbin"
+      "/bin"
+      "/sbin"
+    ];
+
+    environment = {
+      HOST = "0.0.0.0";
+      PORT = "8080";
+    };
+
+    script = ''
+      cd /Users/roo/.config/open-webui/backend
+      ${pkgs.bash}/bin/bash start.sh
+    '';
+
+    serviceConfig = {
+      UserName = "roo";
+      KeepAlive = true;
+      RunAtLoad = true;
+      RootDirectory = "/Users/roo/.config/open-webui/backend";
+      StandardOutPath = "/var/log/open-webui.out.log";
+      StandardErrorPath = "/var/log/open-webui.err.log";
+    };
+  };
+}
